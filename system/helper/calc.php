b4be07752d7f46ae90b4abc7976d477d2b7108dd
Bruno Schmidt <bruno-schmidt@users.noreply.github.com>
2015-12-21 17:26:28 -0200

    conflito

--- system/helper/calc.php
+++ system/helper/calc.php
@@ -3,12 +3,9 @@
 function getDiscountPercentage($price, $special, $floor = true)
 {
   $percentual = (($price - $special) * 100) / $price;
-<<<<<<< HEAD
 
   if($floor)
     return floor($percentual);
 
-=======
->>>>>>> 92b90a0cbd38dc38cc1cba0f4731a200bdfb26bf
   return number_format($percentual, 2, '.', '');
 }
